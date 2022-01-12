37bd558a31e404bc9cecb75345eb4628af70a560
bhupender <bhupender.cool98@gmail.com>
2022-01-12 16:54:22 +0530

    added serverlessmysql

--- config/db.js
+++ config/db.js
@@ -1,24 +1,31 @@
-const { createPool } = require("mysql");
+// const { createPool } = require("mysql");
+const mysql = require("serverless-mysql")();
+// const pool = createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "India@100",
+//   port: 3306,
+//   database: "data",
+// });
 
-const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "India@100",
-  port: 3306,
-  database: "data",
+mysql.config({
+  host: process.env.fhfhf,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
 });
 
-pool.getConnection((err) => {
-  if (err) {
-    console.log("Error conntecting to db...");
-  }
-  console.log("Connected to db...");
-});
+// pool.getConnection((err) => {
+//   if (err) {
+//     console.log("Error conntecting to db...");
+//   }
+//   console.log("Connected to db...");
+// });
 
 const executeQuery = (query, arraParms) => {
   return new Promise((resolve, reject) => {
     try {
-      pool.query(query, arraParms, (err, data) => {
+      mysql.query(query, arraParms, (err, data) => {
         if (err) {
           console.log("error in executing the query");
           reject(err);
